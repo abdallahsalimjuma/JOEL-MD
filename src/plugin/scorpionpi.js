a50dc451401d818b652f2b06f256b6b54adbcc8b
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-11-11 21:21:56 +0300

    Update and rename joelpi.js to scorpionpi.js

--- src/plugin/scorpionpi.js
+++ src/plugin/scorpionpi.js
@@ -20010,7 +20010,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*Pong ${responseTime.toFixed(2)} ms* 🎈`;
+    const text = `*Pong ${responseTime.toFixed(2)} ms* 🦂`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
